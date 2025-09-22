19f2c01e537f84845f1b00a3c040efdcd650e551
oluisfelipe00 <oluisfelipe00@gmail.com>
2025-09-21 21:02:39 -0300

    Update questao1.c

--- Q1/questao1.c
+++ Q1/questao1.c
@@ -3,15 +3,13 @@
 int main() {
     float celsius, fahrenheit;
 
-    // Solicitar o valor em Celsius
     printf("Digite a temperatura em Celsius: ");
     scanf("%f", &celsius);
 
-    // Converter para Fahrenheit
     fahrenheit = (celsius * 9 / 5) + 32;
 
-    // Exibir o resultado
     printf("A temperatura em Fahrenheit é: %.2f\n", fahrenheit);
 
     return 0;
 }
+
