c2a3a930f928f4cb4555e648c3ee68b83493808f
oluisfelipe00 <oluisfelipe00@gmail.com>
2025-09-21 21:03:07 -0300

    Update questao2.c

--- Q2/questao2.c
+++ Q2/questao2.c
@@ -3,7 +3,6 @@
 int main() {
     float salario_base, valor_hora_extra, horas_extras, salario_total;
 
-    // Solicitar dados ao usuário
     printf("Digite o valor do salário base: ");
     scanf("%f", &salario_base);
 
@@ -13,11 +12,10 @@ int main() {
     printf("Digite a quantidade de horas extras: ");
     scanf("%f", &horas_extras);
 
-    // Calcular o salário total
     salario_total = salario_base + (valor_hora_extra * horas_extras);
 
-    // Exibir o resultado
     printf("O valor total a ser recebido é: %.2f\n", salario_total);
 
     return 0;
 }
+
